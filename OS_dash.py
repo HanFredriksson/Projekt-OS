839f4afcb38c3b10375102481deca0b778a35064
Hannes Fredriksson <hanfre@live.se>
2024-11-19 11:41:54 +0100

    Laggt till filer för dashboard och layout

--- OS_dash.py
+++
@@ -1,5 +0,0 @@
-from FetchOSData import OsDataFetcher
-
-os_data = OsDataFetcher("../Projekt-OS/Data/athlete_events.csv", "Name")
-
-ita_os_data = os_data.os_filtered_dataframe("NOC", "ITA")
---
+++ OSdash.py
@@ -0,0 +1,47 @@
+from FetchOSData import OsDataFetcher
+import dash
+
+# The variabels we need
+os_data = OsDataFetcher("../Projekt-OS/Data/athlete_events.csv", "Name")
+ita_os_data = os_data.os_filtered_dataframe("NOC", "ITA")
+
+app = dash.Dash(__name__)
+# Dicts for funktionality of dashboard
+# Dicts for filtering dataframe 
+
+app.layout = html.Div([])
+# How do we want to present our dataset and graphs? 
+# What funktionality do the layout need?
+# What look do we want? 
+
+def drawing_graph():
+    # Graph function needs to have abilty to draw diffrent kinds of graphs
+    pass
+
+
+# Data to visualize for italy 
+def most_medals_per_sport():
+    pass
+
+def number_of_medals_per_os():
+    pass
+
+def another_data_function():
+    pass
+
+# Sports stats data to visualize for 2-4 sports
+def age_spread_in_the_sports():
+    pass
+
+def medla_spread_for_countrys_in_sport():
+    pass
+
+def another_data_function():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
