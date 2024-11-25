ecd316ca3375df2b01b8e466c76a6c0f5de43bfd
Hannes Fredriksson <hannesfre@gmail.com>
2024-11-25 20:37:29 +0100

    [Ränsa/Komnatera] Lagt till lite fler komantarer

--- OSdash.py
+++ OSdash.py
@@ -10,6 +10,7 @@ from plotly.subplots import make_subplots
 
 os_data = OsDataFetcher("../Projekt-OS/Data/athlete_events.csv", "Name")
 
+# Variables with menu options, filters, and color maps.
 picker = {"Italy": "Italy", "Sports": "Sports"}
 ita_filters = {"Total medals over the years": "Year", 
                "Total medals per sport": "Sport", 
@@ -46,6 +47,7 @@ color_map = {"Gold medals":"gold",
 
 app = dash.Dash(__name__)
 
+ # Layout with dropdowns and checklists to let the user filter and plot graphs
 app.layout = html.Div([
     html.H1("OS Data Analyser",style={'textAlign':'center'}),
     html.Div([
@@ -59,7 +61,7 @@ app.layout = html.Div([
             clearable= False
         ),
         
-
+        # Dropdowns and options for sports analysis
         html.Div(
             id="sport-selector", 
             style={"display": "none"}, 
@@ -83,7 +85,7 @@ app.layout = html.Div([
             ]
         ),
       
-
+         # Dropdowns and options for Italy-specific analysis
         html.Div(
             id="italy-selecter", 
             children=[
@@ -109,7 +111,7 @@ app.layout = html.Div([
             ]
             ),
 
-
+        # Dropdown for selecting Olympic season
         html.Div(
             id="season-medals-container",
             children=[
@@ -139,7 +141,7 @@ app.layout = html.Div([
                              'height':'100%'
                              })
 
-
+# Toggels to show or hide menu options
 @app.callback(
     [
         Output("sport-selector", "style"),
@@ -155,19 +157,19 @@ app.layout = html.Div([
 )
 def show_or_hide(selection1, selection2):
     if selection1 == "Sports":
-        # Visa sportval, göm alla andra
+        # Show sport options
         return {"display": "block"}, {"display": "none"}, {"display": "none"}, {"display": "none"}, "Year"  
     
     elif selection1 == "Italy":
-        # Kontrollera vilket filter som valts i italy-medals
+        
         if selection2 == "Sex":
-            # Visa Average Medals
+            # Show graph filter for gender options
             return {"display": "none"}, {"display": "block"}, {"display": "block"}, {"display": "none"}, ""  
         elif selection2 == "Year":
-            # Visa Medals-dropdown
+            # Show graph filter for seasons options
             return {"display": "none"}, {"display": "block"}, {"display": "none"}, {"display": "block"},""
         else:
-            # Standard: Visa bara Italy-selecter
+            # Show Ooptions for graphs for Italy
             return {"display": "none"}, {"display": "block"}, {"display": "none"}, {"display": "none"}, "" 
     
 
@@ -185,12 +187,28 @@ def show_or_hide(selection1, selection2):
 )
 def show_graf(cat, sport, sport_filter, italy_filter, os_season, gender_medals):
 
+    """
+        Generates a graph based on the selected category and filters.
+        
+        Parameters:
+        - cat: Selected category, Italy or Sports.
+        - sport: Selected sport for analysis.
+        - sport_filter: Selected filter for sports.
+        - italy_filter: Selected filter for Italy .
+        - os_season: Selected Olympic season (All, Summer, Winter).
+        - gender_medals: Filter for gender based medal analysis.
+        
+        Returns:
+        - A Plotly figure.
+        """
 
     if cat == "Sports":
         
         if sport_filter == "genderpersport": 
+
             df= medal_counter("Sport", sport, "Sex")
             df.reset_index(inplace=True)
+
             fig= px.bar(df, x= "Sex", y =["Gold medals", "Silver medals", "Bronze medals"],
             barmode="stack", title=f"Medals for each gender in {sport}",
             color_discrete_map={
@@ -198,15 +216,23 @@ def show_graf(cat, sport, sport_filter, italy_filter, os_season, gender_medals):
             "Silver medals":"silver",
             "Bronze medals":"chocolate"
             })
+
+
         elif sport_filter == "gender_participation":
+
             df = gender_participation(os_data.os_filtered_dataframe("Sport", sport), "Year")
+
             fig = px.line(df, x="Year", y="Count", color="Sex",
                           title=f"Gender Participation Over Time in {sport}")
+            
+
         else:
             df = medal_counter("Sport", sport, sport_filter)
+
             if sport_filter == "NOC":
                 df = df[df.values.sum(axis=1) > 5]
                 df.sort_values(by="Total", ascending=True, inplace=True)
+
             fig = px.bar(df, x=df.index, y="Total", title=f"Medals in {sport}")
 
             
@@ -217,6 +243,8 @@ def show_graf(cat, sport, sport_filter, italy_filter, os_season, gender_medals):
         if os_season == "Winter" or os_season == "Summer":
             df = medal_counter("NOC", "ITA", ["Season", "Year"])
             df = df.loc[os_season]
+
+
         if os_season:
             fig = make_subplots(rows=2, 
                                 cols=2, 
@@ -244,23 +272,21 @@ def show_graf(cat, sport, sport_filter, italy_filter, os_season, gender_medals):
         elif gender_medals == "Averages_sex" and italy_filter == "Sex":
             
             df_gender_participation = gender_participation(os_data.os_filtered_dataframe("NOC", "ITA"), "Sport", True)
+
             df = averages(df, italy_filter)
             df.sort_values(by="Sport", inplace=True)
             df_gender_participation.sort_values(by="Sport", inplace=True)
             df = df.merge(df_gender_participation, on=["Sport", "Sex"], how="inner")
             
-            
-          
             bar_trace = go.Bar(
                 x=df['Sport'],
                 y=df['Average Medals'],
                 name='Average Medals',
                 marker_color=df['Sex'].map({'F': 'deepskyblue', 'M': 'tomato'}),
-                text=df['Sex'],  # Display gender on hover
+                text=df['Sex'],
                 hoverinfo="text+y"
             )
 
-            # Line trace for Gender Participation
             line_trace = go.Scatter(
                 x=df['Sport'],  
                 y=df['Count'],
@@ -269,15 +295,13 @@ def show_graf(cat, sport, sport_filter, italy_filter, os_season, gender_medals):
                 line=dict(color='palegreen', width=3),
             )
 
-            # Combine traces into a single figure
             fig = go.Figure(data=[bar_trace, line_trace])
 
-            # Update layout for clarity
             fig.update_layout(
                 title="Average Medals and Gender Participation",
                 xaxis_title="Sport",
                 yaxis_title="Average Medals",
-                barmode='group'  # Group bars by gender
+                barmode='group'
             )
         else:
              fig = px.bar(df, 
@@ -287,13 +311,12 @@ def show_graf(cat, sport, sport_filter, italy_filter, os_season, gender_medals):
                           title=f"Total medals over the {italy_filter}")
              
     return fig
-   
-
-  
 
 
 def averages(data, group):
-    
+
+    # Calculate average medals for a given group
+
     avg_data = (
     data.groupby([group, "Sport"])[["Gold medals", "Silver medals", "Bronze medals", "Total"]].mean()
     .reset_index()
@@ -302,25 +325,16 @@ def averages(data, group):
     move_column = avg_data.pop("Average Medals")
     avg_data.insert(0, "Average Medals", move_column)
 
-    # # Sorting dataframe for most medels on avrage 
-    # sport_totals = avg_data.groupby('Sport')['Average Medals'].sum().reset_index()
-    # sport_totals = sport_totals.rename(columns={'Average Medals': 'Sport Total Medals'})
-
-    # # Reconnect data to orignal data 
-    # avg_data = avg_data.merge(sport_totals, on='Sport')
-
-    # # Sort after total number of averages over gendr and sport
-    # sorted_df = avg_data.sort_values(by=['Sport Total Medals', 'Average Medals'], 
-    #                                  ascending=[False, False],)
     return avg_data
 
-def gender_participation(data, group,medal_sports= False):
-    print(data)
+
+def gender_participation(data, group, medal_sports= False):
+    
+    # Calculate gender participation in sports
+
     if medal_sports:
         
         data = data[data["Medal"].notna()]
-        
-  
 
     participation = data.groupby([group, "Sex"]).size().reset_index(name="Count")
 
@@ -329,6 +343,9 @@ def gender_participation(data, group,medal_sports= False):
 
 
 def medal_counter(pick, filter, count_in):
+
+    # Count medals for a given group
+
     dff = os_data.os_filtered_dataframe(pick, filter)
 
     if count_in == "Sex":
@@ -339,6 +356,7 @@ def medal_counter(pick, filter, count_in):
         f"{medal} medals": dff[dff["Medal"] == medal].groupby(count_in).size()
         for medal in medal_types
     }
+
     medal_count = pd.DataFrame(medal_counts).fillna(0).astype(int)
     medal_count["Total"] = medal_count.sum(axis=1)
 
